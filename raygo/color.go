88d2c1ed0649e3ab604b7c9125b0aec43c9341e6
Danielle Smith <code@danini.dev>
2025-08-29 16:41:43 +0200

    gamma correction

--- raygo/color.go
+++ raygo/color.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 )
 
 var BLACK = Vec3{0.0, 0.0, 0.0}
 var WHITE = Vec3{1.0, 1.0, 1.0}
 
+func linearToGamma(linear_component float64) float64 {
+	if linear_component > 0.0 {
+		return math.Sqrt(linear_component)
+	}
+
+	return 0.0
+}
+
 func WriteColor(out *os.File, pixel_color Vec3) {
-	var r = pixel_color.X
-	var g = pixel_color.Y
-	var b = pixel_color.Z
+	var r = linearToGamma(pixel_color.X)
+	var g = linearToGamma(pixel_color.Y)
+	var b = linearToGamma(pixel_color.Z)
 
 	var intensity = Interval{0.000, 0.999}
 	var rbyte = int(255.0 * intensity.clamp(r))
