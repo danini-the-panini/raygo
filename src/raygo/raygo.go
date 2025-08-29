d8411c8f5c61ac753e15de27c1028ddf33ddce70
Danielle Smith <code@danini.dev>
2025-08-29 13:28:53 +0200

    progress

--- src/raygo/raygo.go
+++ src/raygo/raygo.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
@@ -14,6 +17,7 @@ func main() {
 	fmt.Println("P3\n", image_width, " ", image_height, "\n255")
 
 	for j := range image_height {
+		fmt.Fprint(os.Stderr, "\rScalines remaining: ", (image_height - j), " ")
 		for i := range image_width {
 			var r = float64(i) / float64(image_width-1)
 			var g = float64(j) / float64(image_height-1)
@@ -26,4 +30,6 @@ func main() {
 			fmt.Println(ir, " ", ig, " ", ib)
 		}
 	}
+
+	fmt.Fprint(os.Stderr, "\rDone.                                 ")
 }
